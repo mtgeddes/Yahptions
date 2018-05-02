90b9f1e6b89f70d30daa6d86491a87c01d7a826c
unknown <matthewgeddes@yahoo.com>
2018-05-01 22:40:00 -0400

    Big progress update on javascript, html, and css.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,7 +5,14 @@ $(window).on("load", function(){  // Waits until HTML is loaded before proceedin
     var restaurantsArray = [];
     var radius;
     var zipCode;
-
+    var counter = 0;
+// Button to reload page
+$('#reroll').on("click", function() {
+    if (eliminated.length == 4) {
+        location.reload();
+        console.log("restart")
+    }
+});
 function getLatLongbyZipcode() {
     $('#mainPage').css('opacity', 'unset');
     console.log("getLatLongbyZipcode has been fired!");
@@ -22,7 +29,6 @@ function getLatLongbyZipcode() {
         }
     });
 }
-
 function getLatLongbyNearme() {
     $('#mainPage').css('opacity', 'unset');
      // Begin Ipdata api call; display current zip code
@@ -39,7 +45,6 @@ function getLatLongbyNearme() {
         }
     });
 }
-
 function getRestaurants() {
     console.log("getRestaurants has been fired!");
     console.log("Latitude: " + latitude);
@@ -57,7 +62,6 @@ function getRestaurants() {
                     + "&radius=" + parseInt(radius / 0.00062137) * 1.24  // convert miles to meters with a min of 2,000 M
     var zomatoURL1 = searchURL + "&start=" + page1;
     var zomatoURL2 = searchURL + "&start=" + page2;
-
     $.ajax({  // Fire the api call to get the restaurants; page 1
         url: zomatoURL1,
         dataType: 'json',
@@ -68,7 +72,6 @@ function getRestaurants() {
             for (i = 0; i < data1.restaurants.length; i++) {
                 restaurantsArray.push(data1.restaurants[i]);
             }
-
             $.ajax({  // Fire the api call to get the restaurants; page 2
                 url: zomatoURL2,
                 dataType: 'json',
@@ -86,33 +89,55 @@ function getRestaurants() {
         }
     });
 }
-
 function displayRestaurants() {
     console.log("displayRestaurants has been fired!");
-    var counter = 0;
-    for (i=0; i<=4; i++) {
-        $('.option-' + i).attr('search-result', i);
-        $('.option-' + i + ' h3').text(restaurantsArray[i].restaurant.name);
-        $('.option-' + i + ' img').attr('src', restaurantsArray[i].restaurant.featured_image)
-        $('.option-' + i).append('<h4>' + restaurantsArray[i].restaurant.cuisines + '</h4>').append('<h4>Rating: ' + restaurantsArray[i].restaurant.user_rating.aggregate_rating + '&nbsp;&nbsp;<span class="glyphicon glyphicon-star-empty"></span></h4>');
-
+    for (var i=0; i<=4; i++) {
+        console.log("Counter:" + counter);
+        if ($('.option-' + counter + ' .cardclass').attr('data-state') == "chooseRestaurant" ) {
+            $('.option-' + counter).attr('search-result', counter);
+            $('.option-' + counter + ' h3').text(restaurantsArray[counter].restaurant.name);
+            $('.option-' + counter + ' img').attr('src', restaurantsArray[counter].restaurant.featured_image)
+            $('.option-' + counter).append('<h4 id="cuisines">' + restaurantsArray[counter].restaurant.cuisines + '</h4>').append('<h4 id="rating">Rating: ' + restaurantsArray[counter].restaurant.user_rating.aggregate_rating + '&nbsp;&nbsp;<span class="glyphicon glyphicon-star-empty"></span></h4>');
+            counter++
+        }
     }
 }
-
+function reRoll() {
+    console.log("reRoll has been fired!");
+    z=0;
+    j=0;
+    for (z=0; z<=4; z++) {
+        console.log("z = " + z);
+        console.log("Counter:" + counter);
+        if ($('.option-' + z + ' .cardclass').attr('data-state') == "chooseRestaurant" ) {     
+            $('.option-' + z).attr('search-result', counter);
+        }
+        for (j=0; j<=4; j++) {
+            if ($('.option-' + z).attr('search-result') == counter ) { 
+                console.log("New Resturant if state is true! Counter:" + counter);
+                $('.option-' + z + ' h3').text(restaurantsArray[counter].restaurant.name);
+                $('.option-' + z + ' img').attr('src', restaurantsArray[counter].restaurant.featured_image)
+                $('.option-' + z + ' #cuisines').text(restaurantsArray[counter].restaurant.cuisines)
+                $('.option-' + z + ' #rating').html('Rating: ' + restaurantsArray[counter].restaurant.user_rating.aggregate_rating + '&nbsp;&nbsp;<span class="glyphicon glyphicon-star-empty">');
+                console.log('Next restaurant: ' + restaurantsArray[counter].restaurant.name);  
+            }
+        }
+        counter++;
+    }
+}
+// Reroll options function
+$("#reroll").on("click", reRoll);
 // Closes the search Modal
 $(".close").on("click", function() {
     $("#searchModal").hide();
 });
-
 // Opens the search Modal
 $(".open").on("click", function() {
     $("#searchModal").show();
 });
-
 // Submits the results of the search Modal and hides it
 $("#readyBtn").on("click", function() {
     $("#searchModal").removeClass().addClass("modal show zoomOut animated");
-
     restaurantsArray = [];
     radius = $("#radiusBtn").attr("value") * 8050;
     
@@ -122,76 +147,99 @@ $("#readyBtn").on("click", function() {
         getLatLongbyZipcode();
     }
 })
-
+// Opens search modal
 $("#showModal").on("click", function() {
     $("#searchModal").removeClass().addClass("modal show zoomInDown animated");
     $('#mainPage').css('opacity', '.3');
 });
-
+// Hides search modal
 $("#hideModal").on("click", function() {
     $("#searchModal").removeClass().addClass("modal show zoomOut animated");
     $('#mainPage').css('opacity', 'unset');
 });
-
-// Variables to keep track of chosen and eliminated phases
-var chosen = [];
-var eliminated = [];
-
-function searchrestaurants () {
-
-}
-
-// Choose and eliminate function...
-$(".cardclass").on("click", function() {
-
-    var state = $(this).attr("data-state");
-    
-    if (state ==="chooseRestaurant") {
-        chosen.push("x");
-        $(this).attr("data-state", "lockedin");
-        $(this).addClass("borderchosen");
-        console.log(chosen)
-    }
-
-    else if (state ==="lockedin" && chosen.length == 5) {
-        eliminated.push("x");
-        $(this).css("opacity", "0.2");
-        $(this).removeClass("borderchosen");
-        $(this).addClass("bordereliminated");
-        if (eliminated.length == 4) {
-            chosen = []; 
-            eliminated = [];
-            $("#userSelection").removeClass().addClass("modal show zoomInDown animated");
-            $('#mainPage').css('opacity', '.3');
-            // need to add a clear roll options (maybe)
-        }
-    }
-})
-
-// Closes final chosen restaurant modal
+// Closes the chosen restaurant Modal
 $(".closeUserSelection").on("click", function() {
     $("#userSelection").removeClass().addClass("modal show zoomOut animated");
     $('#mainPage').css('opacity', 'unset');
 });
-
-
+// Variables to keep track of chosen and eliminated phases
+var chosen = [];
+var eliminated = [];
+// Choose and eliminate function...
+$(".cardclass").on("click", function() {
+    var state = $(this).attr("data-state");
+    var eliminationRules = "Great! You've overcome the hardest part. Now, save that friendship and start by eliminating one at a time. Start with what you least desire...<small><small><small>or if you want to start a war, eliminate the one you know your friend really really wants.</small></small></small>"
+    
+    if (state ==="chooseRestaurant") { // Locks in option
+        chosen.push("x");
+        $(this).attr("data-state", "lockedin");
+        $(this).addClass("borderchosen");
+        console.log(chosen)
+        if (chosen.length == 5) {
+            $(".rules").html(eliminationRules)
+            $("#reroll").hide()
+        }
+    }
+    else if (eliminated.length == 4 && state === "lockedin") { // Shows the information on click of last restaurant
+        $("#userSelection").removeClass().addClass("modal show zoomInDown animated");
+        $('#mainPage').css('opacity', '.3');
+    }
+    else if (state ==="lockedin" && chosen.length == 5 && eliminated.length < 5) { // Eliminates option...
+        eliminated.push("x");
+        $(this).css("opacity", "0.2");
+        $(this).removeClass("borderchosen");
+        $(this).removeClass("handpointer");
+        $(this).addClass("bordereliminated");
+        $(this).attr("data-state", "eliminated");
+        if (eliminated.length == 4) {
+            $("#userSelection").removeClass().addClass("modal show zoomInDown animated");
+            $('#mainPage').css('opacity', '.3');
+            $("#reroll").show();
+            $("#reroll").text("Restart");
+        }
+    }
+})
+// Shows user on hover what they're about to lockin
+$(".cardclass").hover(function(){
+    var state = $(this).attr("data-state");
+        if (state ==="chooseRestaurant") {
+            $(this).addClass("borderchosen");
+        }
+    },
+    function () {
+        var state = $(this).attr("data-state");
+        if (state ==="chooseRestaurant") {
+            $(this).removeClass("borderchosen");
+        }
+    }
+);
+// Shows user on hover what they're about to eliminate
+$(".cardclass").hover(function(){
+    var state = $(this).attr("data-state");
+        if (state ==="lockedin" && eliminated.length < 4 && chosen.length == 5) {
+            $(this).addClass("bordereliminated");
+        }
+    },
+    function () {
+        var state = $(this).attr("data-state");
+        if (state ==="lockedin" && eliminated.length < 4 && chosen.length == 5) {
+            $(this).removeClass("bordereliminated");
+        }
+    }
+);
         ////////////////////////////////////////////////////
         //////////// Local storage of favorites ////////////
         //////////////////////////////////////////////////// 
-
 // Creates variable for existing array in storage
 var favoriteList = JSON.parse(localStorage.getItem("favoriteRestaurants"));
-
 // creates local empty favorite list if one doesn't already exist
 if (!Array.isArray(favoriteList)) {
     favoriteList = [];
   }
-
 // Puts favorites on page
 function putFavoritesOnPage() {
     $("#favorites").empty(); // empties out the html
     var insidefavoriteList = JSON.parse(localStorage.getItem("favoriteRestaurants"));
-
     if (!Array.isArray(insidefavoriteList)) {
         insidefavoriteList = [];
     }
@@ -207,24 +255,17 @@ function putFavoritesOnPage() {
         $("#favorites").append(p);
     }
 }
-
 putFavoritesOnPage();
-
 // Deletes past favorites
 $(document).on("click", "button.delete", function() {
     var restaurantList = JSON.parse(localStorage.getItem("favoriteRestaurants"));
     var currentIndex = $(this).attr("data-index");
-
     // Deletes the item marked for deletion
     restaurantList.splice(currentIndex, 1);
     favoriteList = restaurantList;
-
     localStorage.setItem("favoriteRestaurants", JSON.stringify(restaurantList));
-
     putFavoritesOnPage();
 });
-
-
 // Makes a restaurant a favorite
 $(".heart").on("click", function(event) {
     event.preventDefault();
@@ -233,12 +274,6 @@ $(".heart").on("click", function(event) {
     // [Create Later] 
     favoriteList.push(fav); // Will want it to push array of array
     localStorage.setItem("favoriteRestaurants", JSON.stringify(favoriteList));
-
     putFavoritesOnPage();
 });
-
-
-
 }); // <--end of on page load. 
-
-
