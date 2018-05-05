4a379a747406e4674e2e7cb19a903683da208396
unknown <matthewgeddes@yahoo.com>
2018-05-05 02:17:57 -0400

    updated logic. Fixed bugs

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -494,7 +494,7 @@ $(".locationCard").on("click", function() {
         }
     }
     else if (eliminated.length == 4 && state === "lockedin") { // Shows the information on click of last restaurant
-        $("#infoModal-" + $(this).attr("search-result")).removeClass().addClass("modal show zoomInDown animated infoModal");
+        $("#infoModal-" + $(this).attr("data-count")).removeClass().addClass("modal show zoomInDown animated infoModal");
         $('#mainPage').css('opacity', '.3');
     }
     else if (state ==="lockedin" && chosen.length == 5 && eliminated.length < 5) { // Eliminates option...
@@ -504,6 +504,7 @@ $(".locationCard").on("click", function() {
         $(this).removeClass("handpointer");
         $(this).addClass("bordereliminated");
         $(this).attr("data-state", "eliminated");
+        $("#option-info-panel-" + $(this).attr("data-count")).addClass("handpointer").css("opacity", "0.2");;
         if (eliminated.length == 4) {   // Game is over, show the userSelection modal
             for (var i = 0; i <= 4; i++) {
                 var infoPanel = $("#infoModal-" + i);
